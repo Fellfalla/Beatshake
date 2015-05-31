7617b4aa2a1f7bc6f7ac43da7465e6aa3e41ed85
Markus <marweb@alice.de>
2015-05-31 03:44:04 +0200

    It Works

--- README.md
+++
@@ -1,2 +0,0 @@
-# Drivestats
-Eine App um Fahrerbezogen aufgezeichnete Fahrtdaten von einer Datenbank zu laden und aufbereitet wiederzugeben.
---
+++ app/src/main/java/com/fellfalla/beatshake/MainActivity.java
@@ -0,0 +1,97 @@
+package com.fellfalla.beatshake;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private SoundPool soundPool;
+    private int soundID;
+    boolean plays = false, loaded = false;
+    float actVolume, maxVolume, volume;
+    AudioManager audioManager;
+    int counter;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //set the layout of the Activity
+        setContentView(R.layout.activity_main);
+
+        // AudioManager audio settings for adjusting the volume
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        actVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        maxVolume = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        volume = actVolume / maxVolume;
+
+        //Hardware buttons setting to adjust the media sound
+        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+        // the counter will help us recognize the stream id of the sound played  now
+        counter = 0;
+
+        // Load the sounds
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                loaded = true;
+            }
+        });
+        soundID = soundPool.load(this, R.raw.beat, 1);
+
+    }
+
+    public void playSound(View v) {
+        // Is the sound loaded does it already play?
+        if (loaded && !plays) {
+            soundPool.play(soundID, volume, volume, 1, 0, 1f);
+            counter = counter++;
+            Toast.makeText(this, "Played sound", Toast.LENGTH_SHORT).show();
+            plays = true;
+        }
+    }
+
+    public void playLoop(View v) {
+        // Is the sound loaded does it already play?
+        if (loaded && !plays) {
+
+            // the sound will play for ever if we put the loop parameter -1
+            soundPool.play(soundID, volume, volume, 1, -1, 1f);
+            counter = counter++;
+            Toast.makeText(this, "Plays loop", Toast.LENGTH_SHORT).show();
+            plays = true;
+        }
+    }
+
+    public void pauseSound(View v) {
+        if (plays) {
+            soundPool.pause(soundID);
+            soundID = soundPool.load(this, R.raw.beat, counter);
+            Toast.makeText(this, "Pause sound", Toast.LENGTH_SHORT).show();
+            plays = false;
+        }
+    }
+
+    public void stopSound(View v) {
+        if (plays) {
+            soundPool.stop(soundID);
+            soundID = soundPool.load(this, R.raw.beat, counter);
+            Toast.makeText(this, "Stop sound", Toast.LENGTH_SHORT).show();
+            plays = false;
+        }
+    }
+}
