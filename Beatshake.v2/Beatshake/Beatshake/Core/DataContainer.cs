e59a971e567ec04f3e450ec2d44c3d32a0aca58d
Fellfalla <Fellfalla>
2016-05-03 00:27:32 +0200

    Extended DataContainer with conversion to Tuple

--- Beatshake.v2/Beatshake/Beatshake/Core/DataContainer.cs
+++ Beatshake.v2/Beatshake/Beatshake/Core/DataContainer.cs
@@ -1,4 +1,6 @@
-﻿namespace Beatshake.Core
+﻿using System;
+
+namespace Beatshake.Core
 {
     public class DataContainer<TDataType>
     {
@@ -6,6 +8,16 @@
 
         public TDataType[] Rot = new TDataType[3];
 
+
+        public Tuple<TDataType, TDataType, TDataType> TransAsTuple()
+        {
+            return Tuple.Create(Trans[0], Trans[1], Trans[2]);
+        }
+
+        public Tuple<TDataType, TDataType, TDataType> RotAsTuple()
+        {
+            return Tuple.Create(Rot[0], Rot[1], Rot[2]);
+        }
     }
 
 }
