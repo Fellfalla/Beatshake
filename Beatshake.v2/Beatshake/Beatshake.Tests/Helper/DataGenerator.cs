798c018dd68e5d396a2800c9b6d8a03cc4e3d804
Fellfalla <joop03@alice-dsl.net>
2016-05-24 22:08:28 +0200

    - Added several docstrings - refactored ObjectExtensions - removed unused directives - refactored names in Tests

--- Beatshake.v2/Beatshake/Beatshake.Tests/Helper/DataGenerator.cs
+++ Beatshake.v2/Beatshake/Beatshake.Tests/Helper/DataGenerator.cs
@@ -4,7 +4,15 @@ namespace Beatshake.Tests
 {
     public static class DataGenerator
     {
-        public static IEnumerable<double> GetRaisingData(int count, int step = 1, int start = 0)
+        /// <summary>
+        /// This function returns a array with <paramref name="count"></paramref> values 
+        /// beginning at <paramref name="start"/> and by <paramref name="step"></paramref> increasing values
+        /// </summary>
+        /// <param name="count">The number of values in the returne enumerable</param>
+        /// <param name="step">Difference between Two neighboured values</param>
+        /// <param name="start">Value of the first element</param>
+        /// <returns></returns>
+        public static IEnumerable<double> GetData(int count, int step = 1, int start = 0)
         {
             int dataCount = 0;
             for (int i = start; dataCount < count; i += step)
