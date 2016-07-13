67d0559c711265f3344d848498ec4af4322fca8b
Fellfalla <Fellfalla>
2016-07-13 23:27:48 +0200

    commented unused methods out.

--- Beatshake.v2/Beatshake/Beatshake/CoreApplication.xaml.cs
+++ Beatshake.v2/Beatshake/Beatshake/CoreApplication.xaml.cs
@@ -3,6 +3,7 @@ using Beatshake.ViewModels;
 using Beatshake.Views;
 using Microsoft.Practices.Unity;
 using Prism.Common;
+using Prism.Mvvm;
 using Prism.Navigation;
 using Prism.Unity;
 using Prism.Unity.Navigation;
@@ -19,41 +20,41 @@ namespace Beatshake
             //bs.Run(this);
         }
 
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
+        //protected override void OnStart()
+        //{
+        //    // Handle when your app starts
+        //}
 
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
+        //protected override void OnSleep()
+        //{
+        //    // Handle when your app sleeps
 
-        }
+        //}
 
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
-        }
+        //protected override void OnResume()
+        //{
+        //    // Handle when your app resumes
+        //}
 
-        protected Page CreateMainPage()
-        {
+        //protected Page CreateMainPage()
+        //{
 
-            var mainMenuView = Container.Resolve<MainMenuView>();
-            var navPage = new NavigationPage(mainMenuView);
-            //var navPage = new NavigationPage();
-            return navPage;
-        }
-
+        //    var mainMenuView = Container.Resolve<MainMenuView>();
+        //    var navPage = new NavigationPage(mainMenuView);
+        //    //var navPage = new NavigationPage();
+        //    return navPage;
+        //}
         protected override INavigationService CreateNavigationService()
         {
-            //var service = base.CreateNavigationService();
+            //var _navigationService = base.CreateNavigationService();
 
             if (_navigationService == null)
             {
                 var applicationProvider = new ApplicationProvider();
                 _navigationService = new UnityPageNavigationService(Container, applicationProvider, Logger);
-                
+
                 // if _navigationService is not set before MainMenuView is Resolved there will be a endless loop
+                //var mainMenu = Container.Resolve<MainMenuView>();
                 applicationProvider.MainPage = new NavigationPage();//CreateMainPage();
             }
 
@@ -64,6 +65,7 @@ namespace Beatshake
 
         protected override void OnInitialized()
         {
+            InitializeComponent();
             NavigationService.NavigateAsync<MainMenuViewModel>();
         }
 
